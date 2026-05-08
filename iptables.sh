9d211aa9753cad46c317c9518700fe2a2523cbba
Royce-D-O <royceo@unr.edu>
2026-05-08 15:44:16 -0700

    Added sudo to rules

--- iptables.sh
+++ iptables.sh
@@ -3,24 +3,24 @@
 honeypot="192.168.8.100"
 limitBurst=10
 #Accepts packets from users with an already established connection
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Places nmap scanning packets into honeypot
 #Last IP address should be the honeypot IP, currently just a placeholder
-iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL FIN -j DNAT --to-destination "$honeypot"
-iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL SYN -j DNAT --to-destination "$honeypot"
-iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL NONE -j DNAT --to-destination "$honeypot"
-iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL FIN,PSH,URG -j DNAT --to-destination "$honeypot"
+sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL FIN -j DNAT --to-destination "$honeypot"
+sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL SYN -j DNAT --to-destination "$honeypot"
+sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL NONE -j DNAT --to-destination "$honeypot"
+sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL FIN,PSH,URG -j DNAT --to-destination "$honeypot"
 
 #allows internal communication with loopback
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A OUTPUT -o lo -j ACCEPT
 
 #Limits ICMP pings
-iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 3/min --limit-burst "$limitBurst" -j ACCEPT
+sudo iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 3/min --limit-burst "$limitBurst" -j ACCEPT
 
 #accept to port 22, 80, and 443 (Common ports to ACCEPT) (Not sure if these rules are necessary for the purposes of the project)
 #iptables -A INPUT -p tcp -m multiport --dports 22,80,443 -j ACCEPT
 
 # Make make default drop
-iptables -P INPUT -j DROP
+sudo iptables -P INPUT -j DROP
