e106d861df5929d8b39d719ab4a9da5fcba170e4
Nicky <157673541+ohnonick@users.noreply.github.com>
2026-05-08 18:54:17 -0700

    Prettying up iptables
    - Fixing scan strictness
    - More IP-based
    - Pretty 😄

--- iptables.sh
+++ iptables.sh
@@ -1,39 +1,62 @@
 #iptables.sh
-#honeypot IP
+
+# Variables
 honeypot="192.168.8.100"
 limitBurst=5
-#Accepts packets from users with an already established connection
-sudo iptables -N ATTACK
-sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+ext_if="eth0"
+lan_if="eth1"
+internal_net="192.168.4.0/24"
+dmz_net="192.168.8.0/24"
 
-#Places nmap scanning packets into honeypot
-#Last IP address should be the honeypot IP, currently just a placeholder
-sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL FIN -j DNAT --to-destination "$honeypot"
-sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL SYN -j DNAT --to-destination "$honeypot"
-sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL NONE -j DNAT --to-destination "$honeypot"
-sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --tcp-flags ALL FIN,PSH,URG -j DNAT --to-destination "$honeypot"
+echo "Resetting firewall..."
+sudo iptables -F
+sudo iptables -t nat -F
+sudo iptables -X
 
-#Stops arpspoofing by dropping packets from internal networks that are coming from external networks
-sudo iptables -A INPUT -i eth0 -s 192.168.4.0/24 -j DROP
+# Defaults
+sudo iptables -P INPUT DROP
+sudo iptables -P FORWARD DROP
+sudo iptables -P OUTPUT ACCEPT
 
-#allows internal communication with loopback
+# Allowing loopback
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 
-#Limits ICMP pings
-sudo iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 3/min --limit-burst "$limitBurst" -j ACCEPT
+# Allowing existing connections
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-#Limits UPD floods
-sudo iptables -A INPUT -p upd -m limit --limit 10/min --limit-burst 20 -j ACCEPT
+# ICMP limits
+sudo iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 3/min --limit-burst $limitBurst -j ACCEPT
 
-#Accepts port 22 with some limitations
-sudo iptables -A INPUT -p tcp -dport 22 -m state --state NEW -j ATTACK
-sudo iptables -A ATTACK -m recent --set --name ATTACKER
-sudo iptables -A ATTACK -m state --state NEW -m recent --update --seconds 60 --hitcount 3 --name ATTACK-j DROP
+# UDP floods
+sudo iptables -A INPUT -p udp -m limit --limit 10/sec -j ACCEPT
+sudo iptables -A INPUT -p udp -j DROP
 
-#Will limit scans if they bypass the prerouting rules
-iptables -A INPUT -p tcp -m recent --name SCAN --set
-iptables -A INPUT -p tcp -m recent --update --seconds 60 --hitcount 5 --name SCAN -j DROP
+# SSH brute force
+sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSH
+sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 3 --name SSH -j DROP
 
-# Make make default drop
-sudo iptables -A INPUT -j DROP
+# DMZ isolation
+sudo iptables -A FORWARD -s $internal_net -d $dmz_net -j ACCEPT
+sudo iptables -A FORWARD -s $dmz_net -d $internal_net -j DROP
+
+# Scans
+sudo iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -m recent --set --name SCAN
+sudo iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 --name SCAN -j DROP
+sudo iptables -A INPUT -p tcp --tcp-flags ALL FIN -m recent --set --name SCAN
+sudo iptables -A INPUT -p tcp --tcp-flags ALL FIN -m recent --update --seconds 60 --hitcount 5 --name SCAN -j DROP
+sudo iptables -A INPUT -p tcp --tcp-flags ALL NONE -m recent --set --name SCAN
+sudo iptables -A INPUT -p tcp --tcp-flags ALL NONE -m recent --update --seconds 60 --hitcount 5 --name SCAN -j DROP
+sudo iptables -A INPUT -p tcp --tcp-flags ALL FIN,PSH,URG -m recent --set --name SCAN
+sudo iptables -A INPUT -p tcp --tcp-flags ALL FIN,PSH,URG -m recent --update --seconds 60 --hitcount 5 --name SCAN -j DROP
+
+# Honeypot routing
+sudo iptables -t nat -A PREROUTING -i $ext_if -p tcp --syn -m recent --name SCAN --rcheck -j DNAT --to-destination $honeypot
+sudo iptables -t nat -A PREROUTING -i $ext_if -p tcp --tcp-flags ALL FIN -j DNAT --to-destination $honeypot
+sudo iptables -t nat -A PREROUTING -i $ext_if -p tcp --tcp-flags ALL NONE -j DNAT --to-destination $honeypot
+sudo iptables -t nat -A PREROUTING -i $ext_if -p tcp --tcp-flags ALL FIN,PSH,URG -j DNAT --to-destination $honeypot
+
+echo "Firewall is ready!"
+
+# Note: ARP spoofing is Layer 2 (and not handled by iptables)
